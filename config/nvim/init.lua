b1a21b378c1f33bcb61eab8be44f730783db0265
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2023-03-07 12:14:39 -0500

    remove treesitter from nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -10,7 +10,4 @@ require('nvim-autopairs').setup()
 require('lualine').setup()
 require('bufferline').setup {
     options = {show_close_icon  = false, show_buffer_close_icons  = false}}
-require('nvim-treesitter.configs').setup {
-    ensure_installed = {'bash'},
-    highlight = {enable = true}}
 -- Telescope does not need setup
