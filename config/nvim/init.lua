349873df45bde20955a00574764eb3b12edfa0d3
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2023-03-30 17:20:00 -0400

    rewrite nvim config with lazy

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,13 +1,83 @@
-require('settings')
-require('plugins')
-require('keymaps')
+require("settings")
+require("keymaps")
 
-vim.g.catppuccin_flavour = "mocha"
-require("catppuccin").setup()
-vim.cmd [[colorscheme catppuccin]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-require('nvim-autopairs').setup()
-require('lualine').setup()
-require('bufferline').setup {
-    options = {show_close_icon  = false, show_buffer_close_icons  = false}}
--- Telescope does not need setup
+require("lazy").setup({
+
+  {"catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme catppuccin-mocha]])
+    end,
+  },
+
+  {"nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        icons_enabled = false,
+        component_separators = "|",
+        section_separators = "",
+      },
+    },
+  },
+
+  {"akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        show_buffer_icons = false,
+        show_buffer_close_icons  = false,
+        show_close_icon  = false,
+      },
+    },
+  },
+
+  {"windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup()
+    end
+  },
+
+  {"nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      highlight = { enable = true },
+      ensure_installed = {
+        "comment",
+        "markdown",
+        "markdown_inline",
+        "latex",
+        "bash",
+        "lua",
+        "html",
+        "css",
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "rust",
+        "toml",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+
+  {"nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+})
--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -1,28 +1,42 @@
-local keymap = vim.api.nvim_set_keymap
+local keymap = vim.keymap.set
+
+-- Space leader
 vim.g.mapleader = ' '
+
+-- Q functions as q
 keymap('n', 'Q', 'q', {noremap=true})
+
 -- Yank to clipboard
 keymap('v', '<Leader>y', '"+y<cr>', {})
--- Navigate buffers
-keymap('n', '<Leader>l', '<cmd>BufferLinePick<cr>', {})
-keymap('n', '<Leader>n', '<cmd>BufferLineCycleNext<cr>', {})
-keymap('n', '<Leader>p', '<cmd>BufferLineCyclePrev<cr>', {})
--- New buffer, open file
-keymap('n', '<Leader>en', '<cmd>enew<cr>', {})
+
+keymap({'n', 'v'}, '<C-u>', '6k', {})
+keymap({'n', 'v'}, '<C-d>', '6j', {})
+
+-- Navigate window
+keymap('n', '<Leader>wv', '<C-w>v', {})
+keymap('n', '<Leader>wq', '<C-w>q', {})
+keymap('n', '<Leader>wl', '<C-w>l', {})
+keymap('n', '<Leader>wh', '<C-w>h', {})
+keymap('n', '<Leader>wk', '<C-w>k', {})
+keymap('n', '<Leader>wj', '<C-w>j', {})
+
+-- Bufferline
+keymap('n', '<Leader>wn', '<cmd>BufferLineCycleNext<cr>', {})
+keymap('n', '<Leader>wp', '<cmd>BufferLineCyclePrev<cr>', {})
+
+-- Open/Save/Close buffer
 keymap('n', '<Leader>e.', ':e ', {})
 keymap('n', '<Leader>e,', ':e ~/', {})
--- Save buffer
 keymap('n', '<Leader>ew', '<cmd>w<cr>', {})
--- Close buffer, forcefully
-keymap('n', '<Leader>el', '<cmd>bd<cr>', {})
-keymap('n', '<Leader>eL', '<cmd>bd!<cr>', {})
--- Quit nvim, forcefully
-keymap('n', '<Leader>ql', '<cmd>qa<cr>', {})
-keymap('n', '<Leader>qL', '<cmd>qa!<cr>', {})
+keymap('n', '<Leader>ek', '<cmd>bd<cr>', {})
+keymap('n', '<Leader>eK', '<cmd>bd!<cr>', {})
+
 -- Telescope
 keymap('n', '<Leader>ff', '<cmd>Telescope find_files<cr>', {})
 keymap('n', '<Leader>fl', '<cmd>Telescope live_grep<cr>', {})
 keymap('n', '<Leader>fo', '<cmd>Telescope oldfiles<cr>', {})
--- Make C-d and C-u more consistent
-keymap('n', '<C-d>', '12j', {})
-keymap('n', '<C-u>', '12k', {})
+keymap('n', '<Leader>fh', '<cmd>Telescope help_tags<cr>', {})
+
+-- Quit nvim, forcefully
+keymap('n', '<Leader>qq', '<cmd>qa<cr>', {})
+keymap('n', '<Leader>qQ', '<cmd>qa!<cr>', {})
